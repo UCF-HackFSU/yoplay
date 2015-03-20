0036762419a71b87e669116b12edea63c9d6a3d0
tylerl0706 <tylerl0706@gmail.com>
2015-03-20 16:29:19 -0400

    express suks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/frontend'));
+// app.use(express.static(__dirname + '/frontend'));
 
 app.get('/', function(req, res, next) {
   	
