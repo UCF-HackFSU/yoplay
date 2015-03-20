6340774214f4ceeab4ca48077f14df08189cd94b
tylerl0706 <tylerl0706@gmail.com>
2015-03-20 11:58:49 -0400

    html with js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/frontend'));
 
 app.get('/', function(request, response) {
   response.sendfile('frontend/index.html');
