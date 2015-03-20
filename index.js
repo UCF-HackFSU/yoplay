8a7db5be709a71c062c18f1866979b518df747c7
tylerl0706 <tylerl0706@gmail.com>
2015-03-20 16:46:52 -0400

    comments

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var express = require('express');
+//imports 
+var express = require('express'); //express handles GET requests from Yoes
+var request = require('request'); //requests calls the Yo API
+
+//init express stuff
 var app = express();
-var request = require('request');
 
+//init express stuff
 app.set('port', (process.env.PORT || 5000));
-// app.use(express.static('frontend'));
 
+//When we get a GET request
 app.get('/', function(req, res, next) {
   	
-  	
+  	console.log(req.query.username);
   	//Yo
+  	//this if checks if the sent a username to us
   	if(req.query.username != "" || req.query.username != null || req.query.username != {}){
+
+
+		//sends the yo back with a link
 		request.post(
 		    'http://api.justyo.co/yo/',
 		    { form: { 'api_token': '50ebf33f-8bb6-4c76-a9ca-d525324055bc',
@@ -22,58 +30,20 @@ app.get('/', function(req, res, next) {
 		    }
 		);
 	}
+	//end Yo
+
+	//goes to the next fuction
 	next();
 
-	}, function(req, res, next){
-	app.use(express.static('frontend'));
-	res.sendfile('frontend/index.html');
+	}, 
+
+	//this will display index.html found in the frontend folder
+	function(req, res, next){
+		app.use(express.static('frontend'));
+		res.sendFile('frontend/index.html');
 });
 
+//init express stuff
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-
-
-// var http = require('http');
-
-// http.createServer(function (req, res) {
-//   var html = buildHtml(req);
-
-
-//   var https = new XMLHttpRequest();
-// 		var url = "https://f7d0b78f-f308984b5cde.my.apitools.com/yo/";
-// 		var apiToken = "50ebf33f-8bb6-4c76-a9ca-d525324055bc";
-// 		var params = "api_token=" + apiToken + "&username=" + request.query.username;
-// 		https.open("POST", url, true);
-
-// 		//Send the proper header information along with the request
-// 		https.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-// 		https.setRequestHeader("Content-length", params.length);
-// 		https.setRequestHeader("Connection", "close");
-
-// 		http.onreadystatechange = function() {//Call a function when the state changes.
-// 		    if(https.readyState == 4 && http.status == 200) {
-// 		        alert(https.responseText);
-// 		    }
-// 		}
-// 		https.send(params);
-
-//   res.writeHead(200, {
-//     'Content-Type': 'text/html',
-//     'Content-Length': html.length,
-//     'Expires': new Date().toUTCString()
-//   });
-//   res.end(html);
-// }).listen(process.env.PORT || 8080);
-
-// function buildHtml(req) {
-//   var header = '';
-//   var body = 'hello?';
-
-//   // concatenate header string
-//   // concatenate body string
-
-//   return '<!DOCTYPE html>'
-//        + '<html><header>' + header + '</header><body>' + body + '</body></html>';
-// };
