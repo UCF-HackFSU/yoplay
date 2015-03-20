10476c2ccba3bde79b714c347b373010167c9dd8
tylerl0706 <tylerl0706@gmail.com>
2015-03-20 15:12:49 -0400

    html with js

--- index.js
+++ index.js
@@ -4,8 +4,32 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/frontend'));
 
-app.get('/yo', function(request, response) {
-  response.sendfile('frontend/index.html');
+app.get('/', function(request, response) {
+  	response.sendfile('frontend/index.html');
+
+  	//Yo
+  	if(request.query.username != {}){
+	  	
+	  	console.log(request.query.username);
+
+	  	var http = new XMLHttpRequest();
+		var url = "http://api.justyo.co/yo/";
+		var apiToken = "50ebf33f-8bb6-4c76-a9ca-d525324055bc";
+		var params = "api_token=" + apiToken + "&username=" + request.query.username;
+		http.open("POST", url, true);
+
+		//Send the proper header information along with the request
+		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		http.setRequestHeader("Content-length", params.length);
+		http.setRequestHeader("Connection", "close");
+
+		http.onreadystatechange = function() {//Call a function when the state changes.
+		    if(http.readyState == 4 && http.status == 200) {
+		        alert(http.responseText);
+		    }
+		}
+		http.send(params);
+	}
 });
 
 app.listen(app.get('port'), function() {
