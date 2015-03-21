a0853d402060861a4ae6b8242e988d27636e0117
tylerl0706 <tylerl0706@gmail.com>
2015-03-21 03:53:24 -0400

    Change server

--- backend/index.js
+++ backend/index.js
@@ -38,67 +38,67 @@ var uri = "http://localhost:27017";
 // app.set('port', (process.env.PORT || 5000));
 
 //When we get a GET request
-app.get('/', function(req, res, next) {
+// app.get('/', function(req, res, next) {
   	
-  	console.log(req.query.username);
-  	//Yo
-  	//this if checks if they sent a username to us
-  	if(req.query.username != undefined){
+//   	console.log(req.query.username);
+//   	//Yo
+//   	//this if checks if they sent a username to us
+//   	if(req.query.username != undefined){
 
-  		var hasGame = false;
-  		// mongodb.MongoClient.connect(uri, function (err, db) {
-		    /* adventure! */
+//   		var hasGame = false;
+//   		// mongodb.MongoClient.connect(uri, function (err, db) {
+// 		    /* adventure! */
 
-		    var users = db.collection("users");
+// 		    var users = db.collection("users");
 
-		    if(users.find({username:req.query.username}).count() != 0){
-		    	hasGame = true;
-		    }
+// 		    if(users.find({username:req.query.username}).count() != 0){
+// 		    	hasGame = true;
+// 		    }
 		    
-		// });
+// 		// });
 
-		console.log("Username: " + req.query.username + " has game: " + hasGame);
+// 		console.log("Username: " + req.query.username + " has game: " + hasGame);
 
 
-  		if(req.query.location != undefined){
-  			console.log("we got a location! : " + req.query.location);
+//   		if(req.query.location != undefined){
+//   			console.log("we got a location! : " + req.query.location);
 
-  			var lat = req.query.location.split(";")[0];
-  			var lon = req.query.location.split(";")[1];
+//   			var lat = req.query.location.split(";")[0];
+//   			var lon = req.query.location.split(";")[1];
 
-  			//send yo
-  			socket.emit('generate.location', {username:req.query.username, lat:lat, lon:lon});
+//   			//send yo
+//   			socket.emit('generate.location', {username:req.query.username, lat:lat, lon:lon});
 
-  		}
+//   		}
 
-  		var link = 'http://yoplay.x10host.com/?location=' + req.query.location;
-  		console.log("link to use: " + link);
+//   		var link = 'http://yoplay.x10host.com/?location=' + req.query.location;
+//   		console.log("link to use: " + link);
   		
-		//sends the yo back with a link
-		request.post(
-		    'http://api.justyo.co/yo/',
-		    { form: { 'api_token': '50ebf33f-8bb6-4c76-a9ca-d525324055bc',
-		              'username': req.query.username,
-		              'link': link} },
-		    function (error, response, body) {
-		        if (!error && response.statusCode == 200) {
-		            console.log(body);
-		        }
-		    }
-		);
-	}
-	//end Yo
+// 		//sends the yo back with a link
+// 		request.post(
+// 		    'http://api.justyo.co/yo/',
+// 		    { form: { 'api_token': '50ebf33f-8bb6-4c76-a9ca-d525324055bc',
+// 		              'username': req.query.username,
+// 		              'link': link} },
+// 		    function (error, response, body) {
+// 		        if (!error && response.statusCode == 200) {
+// 		            console.log(body);
+// 		        }
+// 		    }
+// 		);
+// 	}
+// 	//end Yo
 
 
-	//goes to the next fuction
-	next();
+// 	//goes to the next fuction
+// 	next();
 
-	}, 
+// 	}, 
 
-	//this will display index.html found in the frontend folder
-	function(req, res, next){
+// 	//this will display index.html found in the frontend folder
+// 	function(req, res, next){
 		
-});
+// });
 
 // //init express stuff
 // app.listen(8888, function() {
