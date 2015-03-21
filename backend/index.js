56fa10d9c66bc128fcd3b950a643bd6128894941
tylerl0706 <tylerl0706@gmail.com>
2015-03-21 14:54:10 -0400

    ya

--- backend/index.js
+++ backend/index.js
@@ -101,7 +101,7 @@ appE.get('/', function(req, res, next) {
 				request.post(
 				    'http://api.justyo.co/yo/',
 				    { form: { 'api_token': '50ebf33f-8bb6-4c76-a9ca-d525324055bc',
-				              'username': data.username,
+				              'username': req.query.username,
 				              'link': link} },
 				    function (error, response, body) {
 				        if (!error && response.statusCode == 200) {
