025162477d05b43ff44d56e3077b5318897392d8
tylerl0706 <tylerl0706@gmail.com>
2015-03-21 04:25:24 -0400

    Change server

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(80);
+app.listen(8888);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
