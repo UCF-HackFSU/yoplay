661cbf9c0fe6955d475268cbccae885d2cfb2779
tylerl0706 <tylerl0706@gmail.com>
2015-03-21 06:18:16 -0400

    b

--- backend/index.js
+++ backend/index.js
@@ -11,33 +11,33 @@ var uri = "yoplay",
 //init express stuff
 var appE = express();
 
-// var app = require('http').createServer(handler)
-// var io = require('socket.io')(app);
-// var fs = require('fs');
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-// app.listen(8888);
+app.listen(8888);
 
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
-// io.on('connection', function (socket) {
-// 	console.log("pls");
+io.on('connection', function (socket) {
+	console.log("pls");
  
 
-//   socket.on('update.location', function (data) {
-//     console.log(data);
-//   });
-// });
+  socket.on('update.location', function (data) {
+    console.log(data);
+  });
+});
 
 
 var game = db.collection("game");
